42d77d4535e426c862f43fc216cf9bb127e30b35
PetroKasperskyi <kaspersky28061993@gmail.com>
2023-06-19 18:54:10 +0300

    lightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,27 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 console.log(galleryItems);
+
+
+const listEl = document.querySelector('.gallery');
+const itemsel = createGalleryItems(galleryItems);
+listEl.insertAdjacentHTML('beforeend', itemsel)
+
+function createGalleryItems(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img  class="gallery__image" src="${preview}" alt="${description}">
+  </a>
+</li>`;
+    }).join("");
+}
+    let lightbox = new SimpleLightbox(".gallery a", {
+        captions: true,
+        captionsData: "alt",
+        captionsDelay: 250,
+        
+    })
